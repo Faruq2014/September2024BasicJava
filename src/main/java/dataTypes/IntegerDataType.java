499d211325f082eca3c9511d085a6fe028f788b7
Mohammed Molla <mjmolla@gmail.com>
2024-11-23 11:27:47 -0500

    how to set up maven project

---
+++ src/main/java/dataTypes/IntegerDataType.java
@@ -0,0 +1,11 @@
+package dataTypes;
+
+public class IntegerDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Java");
+
+	}
+
+}
